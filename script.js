82035d6e8be389212b2d3eebbc4691213280ce83
Julio Schilders <Julio.Schilders@gmail.com>
2019-06-20 12:52:41 +0200

    Removed FontAwesome library & added more svg's
    - Removed FontAwesome library
    - Added more SVG's for SQL, mySQL, etc
    - Added margins on smaller screens
    - Compressed images

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const container = document.querySelector('.container');
+	const anchors = document.querySelectorAll('.anchor');
+
+	
 
 	window.addEventListener('mousemove', event => {
 		container.style.backgroundPositionX = -event.clientX / 75 +'px';
