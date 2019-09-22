2be1afa320d159b7aad244d12b3c2bc7abeb70d8
Julio Schilders <julio.schilders@gmail.com>
2019-09-23 01:17:01 +0200

    Changed projects section, changed colors & images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   const menuIcon = document.querySelector('.menu-icon');
   const menuBar = document.querySelector('.bot-menu');
-  
+
   menuIcon.addEventListener('click', () => {
     menuBar.classList.toggle('open');
     menuBar.classList.toggle('closed');
